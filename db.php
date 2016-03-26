33805a4cb5d7b8b99c02e16e8bcea3986c75c799
Obaid Shah <obaidullah_786@live.com>
2016-03-26 10:02:05 -0700

    this is change made in db removed all below code

--- db.php
+++ db.php
@@ -5,11 +5,3 @@ $username = "root";
 $password = "";
 $dbname = "todolist";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo 'its ok';
-?>
